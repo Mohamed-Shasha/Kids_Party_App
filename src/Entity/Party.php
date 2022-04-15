76b76e9d163b0a3fe724c4a36298d7d9a2e212f9
msnjkmsn <94446637+MShashaIE@users.noreply.github.com>
2022-04-15 02:18:53 +0100

    party entity and templates updates

---
+++ src/Entity/Party.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PartyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PartyRepository::class)]
+class Party
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'integer')]
+    private $priceperhour;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $imagePath;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPriceperhour(): ?int
+    {
+        return $this->priceperhour;
+    }
+
+    public function setPriceperhour(int $priceperhour): self
+    {
+        $this->priceperhour = $priceperhour;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->imagePath;
+    }
+
+    public function setImagePath(string $imagePath): self
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+}
